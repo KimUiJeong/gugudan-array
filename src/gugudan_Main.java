5b9bc0fbc371b1668b255ef6e5b985e62c4a2f96
UiJeong <kui9516@gmail.com>
2020-10-07 04:24:35 +0900

    클래스로 구구단 구현하기

---
+++ src/gugudan_Main.java
@@ -0,0 +1,9 @@
+
+public class gugudan_Main {
+	public static void main(String[] args) {
+		for(int i=2; i < 10; i++) {
+		int[] result = method_gugudan.calculate(i);		
+		method_gugudan.print(result);
+		}
+	}
+}
